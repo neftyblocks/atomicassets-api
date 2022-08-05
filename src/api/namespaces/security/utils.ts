d2806797625ede5597f045b170e988e1bd6d7f08
andreaneftyblocks <andrea@neftyblocks.com>
2022-08-05 19:14:27 +0200

    create utils to parse security method proof of ownership for database storage

---
+++ src/api/namespaces/security/utils.ts
@@ -0,0 +1,51 @@
+import {
+  ProofOfOwnership,
+  ProofOfOwnershipRow,
+  CollectionFilter,
+  TemplateFilter,
+  SchemaFilter,
+  TokenFilter,
+} from "../../../filler/handlers/security/types/tables";
+import {
+  encodeDatabaseJson,
+} from "../../../filler/utils";
+
+export function parseEosioToDBRow(proofOfOwnership: ProofOfOwnership): ProofOfOwnershipRow {
+  const {drop_id, group: {logical_operator, filters}} = proofOfOwnership;
+  const parsedFilters: Array<
+    CollectionFilter|TemplateFilter|SchemaFilter|TokenFilter
+  > = [];
+  filters.map(filter => {
+    const [type, details] = filter;
+    let typedFilter:CollectionFilter|TemplateFilter|SchemaFilter|TokenFilter;
+    switch(type) {
+      case "COLLECTION_HOLDINGS":
+      {
+        const {collection_name, comparison_operator, amount} = details;
+        typedFilter = {collection_name, comparison_operator, amount} as CollectionFilter;
+        break;
+      }
+      case "TEMPLATE_HOLDINGS":
+      { 
+        const {collection_name, template_id, comparison_operator, amount} = details;
+        typedFilter = {collection_name, template_id, comparison_operator, amount} as TemplateFilter;
+        break;
+      }
+      case "SCHEMA_HOLDINGS":
+      {
+        const {collection_name, schema_name, comparison_operator, amount} = details;
+        typedFilter = {collection_name, schema_name, comparison_operator, amount} as SchemaFilter;
+        break;
+      }
+      case "TOKEN_HOLDING":
+        const {token_contract, token_symbol, comparison_operator, amount} = details;
+        const [quantity, symbol] = amount.split(" ");
+        typedFilter = {token_contract, token_symbol, comparison_operator, amount: {quantity, symbol}} as TokenFilter;
+        break;
+      default:
+        throw Error(`Unsupported filter type ${type}, add support for this variant`);
+    }
+    parsedFilters.push(typedFilter);
+  });
+  return {drop_id, logical_operator, filters: encodeDatabaseJson(parsedFilters)};
+}
