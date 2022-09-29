0b7d671e4d07ae294a5b27f8560cb61f7b2e35f5
José Andrade <jandrad@gmail.com>
2022-09-29 13:18:45 +0200

    Transform assets from drops

--- src/api/namespaces/neftydrops/filler.ts
+++ src/api/namespaces/neftydrops/filler.ts
@@ -16,7 +16,7 @@ export class AssetsFiller {
         this.templates = null;
     }
 
-    async fill(templateId: string): Promise<any[]> {
+    async fillTemplate(templateId: string): Promise<any[]> {
         this.query();
 
         const data = await this.templates;
@@ -59,13 +59,24 @@ export async function fillDrops(db: DB, assetContract: string, drops: any[]): Pr
     const templateIds: string[] = [];
 
     for (const drop of drops) {
-        templateIds.push(...drop.templates);
+        const dropTemplateIds: string[] = drop.assets.filter((asset: any) => asset.template_id > -1).map((asset: any) => asset.template_id.toString());
+        templateIds.push(...dropTemplateIds);
     }
 
     const filler = new AssetsFiller(db, assetContract, templateIds, formatAsset, 'atomicassets_templates_master');
 
     return await Promise.all(drops.map(async (drop) => {
-        drop.templates = await filler.fill(drop.templates);
+        drop.assets = await Promise.all(drop.assets.map((asset: any) => {
+            const result: any = {};
+            if (asset.template_id > -1) {
+                result.template = filler.fillTemplate(asset.template_id);
+            } else if (asset.bank_name) {
+                result.bank_name = asset.bank_name;
+            }
+            result.tokens_to_back = asset.tokens_to_back;
+            return result;
+        }));
+        drop.templates = drop.assets.filter((asset: any) => !!asset.template).map((asset: any) => asset.template);
         return drop;
     }));
 }
