306d5dcfa676502ac2cdee61bdd79380ed51ab8e
José Andrade <jandrad@gmail.com>
2023-03-09 22:49:41 +0100

    Add endpoint to query assets that match an ingredient

--- src/api/namespaces/neftyblends/filler.ts
+++ src/api/namespaces/neftyblends/filler.ts
@@ -2,6 +2,7 @@ import {DB} from '../../server';
 import {AssetFiller, FillerHook} from '../atomicassets/filler';
 import {formatTemplate, formatSchema, formatCollection, formatAsset} from '../atomicassets/format';
 import {BlendResultType} from '../../../filler/handlers/blends';
+import {renderMarkdownToHtml} from '../utils';
 
 export class TemplateFiller {
     private templates: Promise<{[key: string]: any}> | null;
@@ -166,10 +167,13 @@ export class CollectionFiller {
     }
 }
 
-export async function fillBlends(db: DB, assetContract: string, blends: any[]): Promise<any[]> {
+export async function fillBlends(db: DB, assetContract: string, blends: any[], renderMarkdown: boolean): Promise<any[]> {
     const templateIds: string[] = [];
     const schemaIds: any[] = [];
     const collectionNames: any[] = [];
+
+
+
     for (const blend of blends) {
         for (const ingredient of blend.ingredients) {
             const templateId = ingredient.template?.template_id;
@@ -197,6 +201,18 @@ export async function fillBlends(db: DB, assetContract: string, blends: any[]): 
                     }
                 }
             }
+        }
+        if (renderMarkdown) {
+            let displayData: Record<string, any> = {};
+            try {
+                displayData = JSON.parse(blend.display_data);
+                if (displayData.description) {
+                    displayData.description = await renderMarkdownToHtml(displayData.description);
+                }
+            } catch (e) {
+                // Ignore
+            }
+            blend.display_data = displayData;
         }
     }
 
