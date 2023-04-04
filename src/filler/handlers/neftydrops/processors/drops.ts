502e73e0a5cb6e4268d4b55a717cfe82850d926a
José Andrade <jandrad@gmail.com>
2023-04-04 21:07:37 +0200

    Fix pair query

--- src/filler/handlers/neftydrops/processors/drops.ts
+++ src/filler/handlers/neftydrops/processors/drops.ts
@@ -280,9 +280,8 @@ export function dropsProcessor(core: NeftyDropsHandler, processor: DataProcessor
               'SELECT pair.invert_delphi_pair, delphi.base_precision, delphi.quote_precision, delphi.median_precision ' +
               'FROM neftydrops_symbol_pairs pair, delphioracle_pairs delphi ' +
               'WHERE pair.listing_symbol = \'USD\' AND pair.settlement_symbol = $1 AND ' +
-              'pair.delphi_contract = delphi.contract AND pair.delphi_pair_name = delphi.delphi_pair_name AND ' +
-              'drop.drops_contract = $2 AND drop.drop_id = $3',
-              [coreSymbol, core.args.neftydrops_account, trace.act.data.drop_id]
+              'pair.delphi_contract = delphi.contract AND pair.delphi_pair_name = delphi.delphi_pair_name' +
+              [coreSymbol]
             );
 
             if (query.rowCount === 0) {
