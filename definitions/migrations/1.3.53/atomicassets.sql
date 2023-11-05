23627fc0d611d2b21cf1216d58afadcc6517bf5e
José Andrade <jandrad@gmail.com>
2023-11-05 11:13:57 +0100

    Add missing drop in migration

--- definitions/migrations/1.3.53/atomicassets.sql
+++ definitions/migrations/1.3.53/atomicassets.sql
@@ -1,3 +1,4 @@
 DROP VIEW IF EXISTS atomicmarket_assets_master;
 DROP VIEW IF EXISTS neftymarket_assets_master;
 DROP VIEW IF EXISTS atomicassets_assets_master;
+DROP VIEW IF EXISTS atomicassets_templates_master;
