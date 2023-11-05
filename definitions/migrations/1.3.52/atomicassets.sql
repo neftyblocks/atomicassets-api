d3c99df7d0786cfa689c4b6753530be6a704ffbe
José Andrade <jandrad@gmail.com>
2023-11-05 09:37:05 +0100

    Include pack in template Include recipe id in pack

---
+++ definitions/migrations/1.3.52/atomicassets.sql
@@ -0,0 +1,3 @@
+DROP VIEW IF EXISTS atomicmarket_assets_master;
+DROP VIEW IF EXISTS neftymarket_assets_master;
+DROP VIEW IF EXISTS atomicassets_assets_master;
--- src/filler/handlers/packs/types/tables.ts
+++ src/filler/handlers/packs/types/tables.ts
@@ -4,5 +4,6 @@ export type PacksTableRow = {
     unlock_time: number,
     pack_template_id: number,
     use_count: number,
+    recipe_id?: number,
     display_data: string,
 };
