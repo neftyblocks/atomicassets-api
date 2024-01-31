ec751ccc875f177011fe7dcbbcc49d3cf7af04cd
José Andrade <jandrad@gmail.com>
2024-02-01 00:06:12 +0100

    Fix atomicpacks init

--- src/filler/handlers/packs/processors/packs.ts
+++ src/filler/handlers/packs/processors/packs.ts
@@ -34,6 +34,20 @@ const fillPacks = async (args: PacksArgs, connection: ConnectionManager, contrac
         }
 
         await bulkInsert(connection.database, 'neftypacks_packs', packDbRows);
+
+        const atomicContract = args.atomic_packs_account.trim();
+        if (contract === atomicContract) {
+            await connection.database.query('UPDATE neftypacks_packs p ' +
+                'SET use_count = usage.count ' +
+                'FROM (' +
+                'SELECT p.pack_id, p.contract, COUNT(*) count FROM neftypacks_packs p ' +
+                'INNER JOIN atomicassets_assets a ON burned_by_account = p.contract AND a.template_id = p.pack_template_id ' +
+                'WHERE p.contract = $1 AND p.pack_template_id != -1 ' +
+                'GROUP BY p.pack_id, p.contract ' +
+                ') usage ' +
+                'WHERE p.pack_id = usage.pack_id AND p.contract = usage.contract;', [atomicContract]
+            );
+        }
     }
 };
 
@@ -43,17 +57,6 @@ export async function initPacks(args: PacksArgs, connection: ConnectionManager):
     await fillPacks(args, connection, neftyContract);
     if (atomicContract) {
         await fillPacks(args, connection, atomicContract);
-
-        await connection.database.query('UPDATE neftypacks_packs p ' +
-            'SET use_count = usage.count ' +
-            'FROM (' +
-            'SELECT p.pack_id, p.contract, COUNT(*) count FROM neftypacks_packs p ' +
-            'INNER JOIN atomicassets_assets a ON burned_by_account = p.contract AND a.template_id = p.pack_template_id ' +
-            'WHERE p.contract = $1 AND p.pack_template_id != -1 ' +
-            'GROUP BY p.pack_id, p.contract ' +
-            ') usage ' +
-            'WHERE p.pack_id = usage.pack_id AND p.contract = usage.contract;', [atomicContract]
-        );
     }
 }
 
