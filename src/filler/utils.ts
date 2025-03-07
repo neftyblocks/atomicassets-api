727d889ccd0dfd73ded53d29692fb96e45e70094
José Andrade <jandrad@gmail.com>
2025-03-07 17:14:59 +0100

    Attempt to fix db issue

--- src/filler/utils.ts
+++ src/filler/utils.ts
@@ -13,7 +13,9 @@ export function encodeDatabaseJson(obj: any): string {
 export function encodeDatabaseArray(array: any[]): string {
     const data = array.map(x => {
         if (typeof x === 'string') {
-            return x.replace(/"/g, '\\"').replace(/{/g, '\\{');
+            return x.replace(/"/g, '\\"')
+                    .replace(/{/g, '\\{')
+                    .replace(/\\./g, '\\\\.');
         }
         return x;
     });
